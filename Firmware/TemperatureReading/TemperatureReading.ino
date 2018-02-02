98ab033edbb04d8484d16cf95c7f24d142820d39
James Garcia-Otero <jg4ye@virginia.edu>
2018-02-01 22:51:35 -0500

    removed LCD software from TemperatureReading

--- Firmware/TemperatureReading/TemperatureReading.ino
+++ Firmware/TemperatureReading/TemperatureReading.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+const int sensorPin = A0;
 
+void setup() {
+  // Set up serial connection speed
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Read the sensor value
+  int sensorVal = analogRead(sensorPin);
+  // Convert reading to voltage
+  float voltage = (sensorVal/1024.0) * 5.0;
+  //convert milivolts into temperature
+  float temperature = (voltage - 0.5) * 100;
+  Serial.print(temperature);
+  Serial.print(",");
+  delay(1000);
 }
