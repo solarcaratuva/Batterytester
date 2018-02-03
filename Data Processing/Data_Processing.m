9ce88c478bc4f57cbca3bdcaf4256dfeea2233d6
Daniel Ayoub <dsa8cx@virginia.edu>
2018-02-03 13:47:22 -0500

    adding file readers

--- Data Processing/Data_Processing.m
+++ Data Processing/Data_Processing.m
@@ -19,3 +19,24 @@ plot(Time, E)
 MatrixC = Matrix(:,3);
 plot(Time,Voltage,'y-',Time,MatrixC,'go')
 %}
+
+
+%Function: to accurately parse CSV data obtained from the SD card 
+
+%Matrix for the voltages, current, and temperature where each column is a
+%seperate battery 
+
+filename = ' '; %insert file name  
+num_batteries = 3; 
+input_file = fopen(filename,'r');
+if (input_file ~= -1)
+    if (contains(filename,'csv'))
+        all_data=csvread(filename);
+    else
+        all_data=xlsread(filename);
+    end
+    
+    for n=1; num_batteries
+        voltage_data = all_data(:, n);
+    end 
+end
