8030e184d675ea8b459ab3a845c47dcea97f3dbf
Karan Chawla <ksc3ft@virginia.edu>
2018-02-03 15:37:15 -0500

    Updated Battery Indices
    Battery Indices are now correlated to A1-A36,B1-B36...M1-M36 in a matrix called indexCells. The actual battery values are in a matrix called pack.

--- Data Processing/Capacities_to_Battery_Pack.m
+++ Data Processing/Capacities_to_Battery_Pack.m
@@ -20,7 +20,8 @@ end
 
 avg = tot/416;
 pack = zeros(13,32); %Sets up empty matrix
-
+letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N"];
+indexCells = {};
 for i = 1:2:13 %iterates over every other slot in a pack
    for j = 1:32 %iterates through each battery pack
        pack(i,j) = caps(count); %Adds low value capacity to first pack
@@ -37,6 +38,23 @@ for i = 2:2:12 %iterates over every other slot in a pack
         count = count + 1;
     end
 end
-%Pack is the pack of batteries, Pack2 is the number of the battery (1-416).
+
+for i = 1:13
+    for j = 1:32
+        x = pack2(i,j);
+        m = 0;
+        f = x;
+        while(x > 32)
+            m = m+1;
+            x = x-32;
+        end
+        indexCells{i,j} = letters(i)+string(x);
+    end
+end
+
+
+%Pack is the pack of batteries, pack2 is the number of the battery (1-416).
+%IndexCells contains the ordered definition of the batteries
+%(A1-A36,B1-B36,etc...)
 %avg
-sums = sum(pack/13)
+%sums = sum(pack/13)
