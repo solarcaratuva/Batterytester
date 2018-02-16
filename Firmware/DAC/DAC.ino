77f983ec35a662727b05309c034be4d55d81c3b0
Adam El Sheikh <AdamElSheikh99@gmail.com>
2018-02-15 20:06:44 -0500

    Library For Our MCP4822 DAC
    Included the MCPDAC library and some code that implements it

---
+++ Firmware/DAC/DAC.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include "MCPDAC.h"
+
+/*
+ * Pin 2 = CSPin = 10
+ * Pin 3 = SCK = 13
+ * Pin4 = SDI = 11
+ * Pin 8 = VoutA
+ * Pin 6 = VoutB
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+  MCPDAC.begin();
+
+  MCPDAC.setGain(CHANNEL_A,GAIN_HIGH);
+
+  // Do not shut down channel A, but shut down channel B.
+  MCPDAC.shutdown(CHANNEL_A,false);
+  //MCPDAC.shutdown(CHANNEL_B,true);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned int mv = 1000;
+  MCPDAC.setVoltage(CHANNEL_A, mv&0x0FFF);
+}
