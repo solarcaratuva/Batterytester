118fbeb650764b2045fee7e94a95971052902c95
maximpoe <mep9cf@virginia.edu>
2018-03-25 18:52:39 -0400

    fixed bugs associated with switch found bugs with discharger.h possible bugs with decoder.h
       |           |
        \  \___/  /
         \ /   \ /
      ----|     |-----
         / \___/ \
        /         \
       |          |

--- Firmware/newBatteryTester/misc.h
+++ Firmware/newBatteryTester/misc.h
@@ -6,9 +6,11 @@
 
 
 #define FILENAME "batchA.csv"
-#define BATCH_SIZE 2              // batch size represents the number of cells we are currently testing
+#define BATCH_SIZE 30              // batch size represents the number of cells we are currently testing
 #define SD_CS_PIN 10              // Chip Select Pin for SD Card Reader
 #define RTC_INTERRUPT_PIN 2       // RTC Will be connected to pin D2 to trigger interrupt every 1 second
 #define THRESHOLD_TEMP 700
+#define DISCHARGE_CURRENT 3000    // This is the discharge rate we plan to test the boards with
+
 
 #endif
--- Firmware/newBatteryTester/newBatteryTester.ino
+++ Firmware/newBatteryTester/newBatteryTester.ino
@@ -5,9 +5,11 @@
 #include "misc.h"
 #include "discharger.h"
 #include "MCPDAC.h"
+#include "RTClib.h"
+#include "mcp3208.h"
+RTC_DS1307 rtc;
 // constants located in misc.h
 
-#include "mcp3208.h"
 
 /*
   Arduino SPI (SD card, DAC, ADC):
@@ -23,12 +25,11 @@
 */
 
 
-
 int cell[3] = {0,0,0}; // {voltage,current,tempurature}
 volatile uint32_t time_flag = 0; // this gets incremented by the interrupt when it's triggered. Should be reset to zero when it's handled.
 uint32_t timestamp = 0; // Counts the number of seconds that have passed (it is never reset unless arduino is reset)
 File myFile;
-uint32_t I_set = 3000; // value going to DAC TODO: Allow this to be configurable over serial port
+uint32_t I_set = 0; // discharge current
 
 bool loggingState = false;
 bool tempThresholdExceeded = false;
@@ -44,6 +45,7 @@ void setup() {
   Serial.begin(115200);
   SPI.begin();
   SPI.setClockDivider(0);
+  rtc.begin();
   pinMode(A0, INPUT);
   decoder::init(); // set pinmodes for decoder (this is hardcoded for performance)
   MCPDAC.begin(9);
@@ -51,23 +53,11 @@ void setup() {
   MCPDAC.setGain(CHANNEL_B,GAIN_HIGH);
   MCPDAC.shutdown(CHANNEL_A,false);
   MCPDAC.shutdown(CHANNEL_B,false);
-  MCPDAC.setVoltage(CHANNEL_A, I_set/2); // div/2 is because discharge_current (A) = 2*I_SET.
   MCPDAC.setVoltage(CHANNEL_B, 2500); // 2.5V gets multiplied by the x2 amp
+  rtc.writeSqwPinMode(SquareWave1HZ); // make sure RTC is configured properly
   while (!Serial){}
   
-  /* ISR is attached to RTC_INTERUPT_PIN, which is
-   *  connected to SQR output on RTC. time_flag
-   *  is incremented every second and decremented once
-   *  things are taken care of.
-   */
-  pinMode(RTC_INTERRUPT_PIN, INPUT); // configure this pin as an INPUT
-  attachInterrupt(
-    digitalPinToInterrupt(RTC_INTERRUPT_PIN),
-    [](void){
-      (PINC&1) ? (time_flag++) : (I_set = 0) ;
-      }, // increment time_flag if buttonState is 1
-    RISING
-  );
+
 
   // initialize the SD and write the header to the CSV as nessesary:
   Serial.println(F("Initializing SD card..."));
@@ -77,13 +67,15 @@ void setup() {
   }
   Serial.println(F("Initialization done"));
 
-
   // if file exists yet, just open it:
   if (SD.exists(FILENAME)) {
     myFile = SD.open(FILENAME, FILE_WRITE);
-  } else {
+    
+  }
+  else {
     //create labels for batteries
     myFile = SD.open(FILENAME, FILE_WRITE);
+    myFile.print("time,");
     for (int x = 1; x <= BATCH_SIZE; x++) {
       myFile.print("Cell " + String(x) + " Voltage(V),");
       myFile.print("Cell " + String(x) + " Current(A),");
@@ -91,47 +83,44 @@ void setup() {
     }
     myFile.println(); // go down to next row
     myFile.close(); // close the newly created empty file
-    Serial.println(String("Created file: ") + String(FILENAME));
+    Serial.print(F("Created file: "));
+    Serial.println(F(FILENAME));
   }
+   /* ISR is attached to RTC_INTERUPT_PIN, which is
+   *  connected to SQR output on RTC. time_flag
+   *  is incremented every second and decremented once
+   *  things are taken care of.
+   */
+  pinMode(RTC_INTERRUPT_PIN, INPUT); // configure this pin as an INPUT
+  attachInterrupt(
+    digitalPinToInterrupt(RTC_INTERRUPT_PIN),
+    [](void){time_flag++;},
+    RISING
+  );
 }
 void loop(){
   if(time_flag > 0){
-    //Serial.println(Discharger::read_voltage(0));
-    myFile = SD.open(FILENAME, FILE_WRITE);
-    myFile.print(millis()); myFile.print(',');
-    for (int i = 0; i < BATCH_SIZE; i++) {
-      cell[0] = Discharger::read_voltage(i);
-      cell[1] = Discharger::read_current(i);
-      cell[2] = Discharger::read_temp(i);
-      myFile.print(cell[0]); myFile.print(',');
-      myFile.print(cell[1]); myFile.print(',');
-      myFile.print(cell[2]); myFile.print(','); 
+    if(PINC&1/*A0==true*/){
+      MCPDAC.setVoltage(CHANNEL_A, DISCHARGE_CURRENT/2); // discharge_current (Amps) = 2*I_SET.
+      myFile = SD.open(FILENAME, FILE_WRITE);
+      myFile.print(millis()); myFile.print(',');
+      for (int i = 0; i < BATCH_SIZE; i++) {
+        cell[0] = Discharger::read_voltage(i);
+        cell[1] = Discharger::read_current(i);
+        cell[2] = Discharger::read_temp(i);
+        myFile.print(cell[0]); myFile.print(',');
+        myFile.print(cell[1]); myFile.print(',');
+        myFile.print(cell[2]); myFile.print(',');
+      }
+      myFile.println();
+      myFile.close();
+      Serial.println(F("wrote stuff to SD card"));
     }
-    myFile.println();
-    myFile.close();
-    Serial.println("wrote stuff to SD card");
+    else{
+      MCPDAC.setVoltage(CHANNEL_A, 0); // discharge_current (Amps) = 2*I_SET.
+      Serial.println(F("BatteryTester off"));
+    }
     time_flag--;
   }
 }
-
-
-// returns true if tempurature exceeds limit
-//boolean tempExceeded(uint16_t curTemp) {
-//  return (curTemp > THRESHOLD_TEMP);
-//}
-
-//void readWrite() {
-//  if(tempExceeded()) {
-//    Serial.print("Temp threshold exceeded");
-//    return;
-//  }
-//  // attempt to write loads of line
-//  myFile = SD.open(FILENAME, FILE_WRITE);
-//  for(uint8_t i = 0; i > BATCH_SIZE; i++){
-//    cell[0] = 
-//    writer.write(myFile, cell, timestamp);
-//  }
-//  myFile.close();
-//
-//}
 
